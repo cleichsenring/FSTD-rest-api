a1d6c9402ea6b9ff06f4371fb79678e708c0ece2
Chris Leichsenring <5677805+cleichsenring@users.noreply.github.com>
2020-10-03 19:30:07 -0400

    user auth

--- routes.js
+++ routes.js
@@ -2,22 +2,14 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
 const { Course , User } = require('./models');
+const bcrypt = require('bcryptjs');
+const auth = require('basic-auth');
 
 const router = express.Router();
 
-/* User routes */
-router.get('/users', asyncHandler(async (req, res) => {
-
-}));
-
-router.post('/users', (req, res) => {
-  res.status(201).set('Location', '/').end();
-});
-
-
 const filterOptions = {
   include:[{ 
-    model: User, 
+    model: User,
     attributes: { 
       exclude: ['password', 'createdAt', 'updatedAt']
     } 
@@ -27,6 +19,72 @@ const filterOptions = {
   }
 }
 
+// User authentication
+const authenticateUser = async (req, res, next) => {
+  let message = null;
+
+  const credentials = auth(req);
+
+  if (credentials){
+    const user = await User.findOne({ where: { emailAddress: credentials.name } });
+    if (user) {
+      const authenticated = bcrypt.compareSync(credentials.pass, user.password);
+
+      if (authenticated) {
+        console.log(`Authentication successful for user: ${user.emailAddress}`);
+        req.currentUser = user;
+      } else {
+        message = `Authentication failed for user: ${user.emailAddress}`
+      }
+
+    } else {
+      message = `No user with email: ${credentials.name} found.`
+    }
+
+  } else {
+    message = 'Authorization required!'
+  }
+
+  if (message) {
+    console.warn(message);
+    res.status(401).json({ message: 'Access Denied' });
+  } else {
+    next();
+  }
+}
+
+/* User routes */
+router.get('/users', authenticateUser, asyncHandler(async (req, res) => {
+  const user = await User.findOne({
+    where: {
+      emailAddress: req.currentUser.emailAddress
+    },
+    attributes: {
+      exclude: ['password', 'createdAt', 'updatedAt']
+    }
+  });
+  res.json(user);
+}));
+
+router.post('/users', asyncHandler(async (req, res) => {
+  try {
+    let { firstName, lastName, emailAddress, password } = req.body;
+    password = bcrypt.hashSync(password, 10);
+    await User.create({ firstName, lastName, emailAddress, password })
+    res.status(201).set('Location', '/').end();
+  } catch (error) {
+    if (error.name === 'SequelizeValidationError') {
+      const errors = error.errors.map( err => err.message );
+      console.error('Validation errors: ', errors);
+      res.status(400).json({"Errors": errors});
+    } else {
+      throw error;
+    }
+  }
+}));
+
+
+
 /* Courses routes */
 router.get('/courses', asyncHandler(async (req, res) => {
   const courses = await Course.findAll(filterOptions);
@@ -35,32 +93,51 @@ router.get('/courses', asyncHandler(async (req, res) => {
 
 router.get('/courses/:id', asyncHandler(async (req, res) => {
   const course = await Course.findByPk(req.params.id, filterOptions );
-  course 
+  (course) 
     ? res.json(course) 
     : res.status(404).json({message: `Course id: ${req.params.id} not found`});
 }));
 
-router.post('/courses', asyncHandler(async (req, res) => {
-  const { title , description, userId } = req.body;
-  // Getting error "message": "SQLITE_CONSTRAINT: FOREIGN KEY constraint failed"
-  const course = await Course.create(req.body);
-  res.status(201).set('Location', `/courses/`).end();
+router.post('/courses', authenticateUser, asyncHandler(async (req, res, next) => {
+  try {
+    const course = await Course.create(req.body);
+    res.status(201).set('Location', `/courses/${course.id}`).end();  
+  } catch (error) {
+    if (error.name === 'SequelizeValidationError') {
+      const errors = error.errors.map( err => err.message );
+      console.error('Validation errors: ', errors);
+      res.status(400);
+    } else {
+      throw error;
+    }
+  }
 }));
 
-router.put('/courses/:id', asyncHandler(async (req, res) => {
+router.put('/courses/:id', authenticateUser, asyncHandler(async (req, res) => {
   const course = await Course.findByPk(req.params.id);
   if (course) {
-
+    try {
+      await course.update(req.body);
+      res.status(204).end();
+    } catch (error) {
+      if (error.name === 'SequelizeValidationError') {
+        const errors = error.errors.map( err => err.message );
+        console.error('Validation errors: ', errors);
+        res.status(400).json({"Errors": errors});
+      } else {
+        throw error;
+      }
+    }
   } else {
     res.status(404).json({message: `Course id: ${req.params.id} not found`});
   }
 }));
-router.delete('/courses/:id', asyncHandler(async (req, res) => {
+
+router.delete('/courses/:id', authenticateUser, asyncHandler(async (req, res) => {
   const course = await Course.findByPk(req.params.id);
   if (course) {
     await course.destroy();
     res.status(204).end();
-
   } else {
     res.status(404).json({message: `Course id: ${req.params.id} not found`});
   }
